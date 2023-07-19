94bac939ecd7ffa165fcb59d6cc2991f78b31009
Arif İskilip <71379630+arifiskilip@users.noreply.github.com>
2023-07-19 13:26:18 +0300

    Jwt

---
+++ WebUI/Auth/TokenGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace WebUI.Auth
+{
+    public static class TokenGenerator
+    {
+        public static string GenerateToken()
+        {
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysecretkey123."));
+            SigningCredentials credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            JwtSecurityToken tokent = new JwtSecurityToken(issuer:"https://localhost",audience: "https://localhost",notBefore:DateTime.Now,expires:DateTime.Now.AddMinutes(10),signingCredentials:credentials);
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+            return handler.WriteToken(tokent);
+         }
+    }
+}
--- WebUI/Startup.cs
+++ WebUI/Startup.cs
@@ -1,12 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebUI
@@ -23,6 +26,19 @@ namespace WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.RequireHttpsMetadata = true;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = "https://localhost",
+                    ValidAudience = "https://localhost",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("mysecretkey123.")),
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
             services.AddControllersWithViews();
             services.AddHttpClient();
         }
